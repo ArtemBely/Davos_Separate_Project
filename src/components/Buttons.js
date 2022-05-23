8681645b77f368a3169259bc9ea05a76dc8da024
Артем Белышев <you@example.com>
2022-05-23 13:28:59 +0200

    program2

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,7 +6,7 @@ function Buttons(){
          <div className="wrap_buttons">
             <div className='buttons_hub'>
                 <a href='https://drive.google.com/file/d/1b48QbqQUKk4n2GR9_u0aCSkFw1nlyjWV/view?usp=sharing' className='hub_links'><p className='hub_menu'>Menu</p></a>
-                <a href='https://drive.google.com/file/d/1YDpuJAb0jKM2ifh-eGvh2FxO2krZWSC0/view?usp=sharing' className='hub_links'><p className='hub_menu'>Program</p></a>
+                <a href='https://drive.google.com/file/d/1rNS-4EQNpk3_00pNKzUfK2phkm-kYKFQ/view?usp=sharing' className='hub_links'><p className='hub_menu'>Program</p></a>
     				</div>
          </div>
 
